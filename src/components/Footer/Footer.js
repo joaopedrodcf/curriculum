6e529efbe96e1f3a2b51f1a7333812cca674ea91
joaopedrodcf <joaoferr93@gmail.com>
2018-05-14 13:47:55 +0100

    Footer now centered

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,15 +3,15 @@ import React from 'react';
 // https://coolors.co/ffc857-e9724c-c5283d-481d24-255f85
 const Footer = () => (
   <footer>
-    <div>© Copyright João Ferreira 2018</div>
     <div>
       <a href="https://github.com/joaopedrodcf">
-        <i className="fab fa-github fa-3x" />
+        <i className="fab fa-github fa-2x" />
       </a>
       <a href="https://www.linkedin.com/in/joaoferr93/">
-        <i className="fab fa-linkedin fa-3x" />
+        <i className="fab fa-linkedin fa-2x" />
       </a>
     </div>
+    <div>© Copyright João Ferreira 2018</div>
   </footer>
 );
 
