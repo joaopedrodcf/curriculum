b1d6a42e451a04b4f39d1c3d8985da2a0da3294e
joaopedrodcf <joaoferr93@gmail.com>
2018-05-14 13:17:47 +0100

    Improving footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,6 +3,7 @@ import React from 'react';
 // https://coolors.co/ffc857-e9724c-c5283d-481d24-255f85
 const Footer = () => (
   <footer>
+    <div>© Copyright João Ferreira 2018</div>
     <div>
       <a href="https://github.com/joaopedrodcf">
         <i className="fab fa-github fa-3x" />
